bea57c683ecff59a8c33a2e175de2d901f4cb41f
Orel bukris <orelbukris7777@gmail.com>
2025-05-27 09:08:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+function App() {
+  return (
+    <>
+    <main className='bg-slate-300/20'>
+    <h1 className='text-red-500'>hello</h1>
+
+    </main>
+    </>
+  )
+}
+
+export default App
