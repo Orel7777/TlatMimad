2f04d0df42bef3f2ee571c658df8021d738ecf57
Orel bukris <orelbukris7777@gmail.com>
2025-05-29 10:42:20 +0300

    loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,103 @@
-import {Html} from '@react-three/drei';
+import { Html } from '@react-three/drei';
 import React from 'react';
+import styled from 'styled-components';
+
 const Loader = () => {
   return (
-    <Html>
-    <div className='flex items-center justify-center w-full h-full bg-gray-800'>
-        <h1>
-            Loading....
-        </h1>
-    </div>
-
+    <Html fullscreen>
+      <Container>
+        <StyledWrapper>
+          <div className="loader">
+            <span />
+          </div>
+        </StyledWrapper>
+      </Container>
     </Html>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+
+const StyledWrapper = styled.div`
+  transform: scale(1.5);
+  
+  .loader {
+    position: relative;
+    width: 150px;
+    height: 150px;
+    background: transparent;
+    border-radius: 50%;
+    box-shadow: 25px 25px 75px rgba(0,0,0,0.55);
+    border: 1px solid #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+  }
+
+  .loader::before {
+    content: '';
+    position: absolute;
+    inset: 20px;
+    background: transparent;
+    border: 1px dashed#444;
+    border-radius: 50%;
+    box-shadow: inset -5px -5px 25px rgba(0,0,0,0.25),
+    inset 5px 5px 35px rgba(0,0,0,0.25);
+  }
+
+  .loader::after {
+    content: '';
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px dashed#444;
+    box-shadow: inset -5px -5px 25px rgba(0,0,0,0.25),
+    inset 5px 5px 35px rgba(0,0,0,0.25);
+  }
+
+  .loader span {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 50%;
+    height: 100%;
+    background: transparent;
+    transform-origin: top left;
+    animation: radar81 2s linear infinite;
+    border-top: 1px dashed #fff;
+  }
+
+  .loader span::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: seagreen;
+    transform-origin: top left;
+    transform: rotate(-55deg);
+    filter: blur(30px) drop-shadow(20px 20px 20px seagreen);
+  }
+
+  @keyframes radar81 {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default Loader
